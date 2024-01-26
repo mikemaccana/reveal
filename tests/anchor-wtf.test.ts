da5c50b1a8672c932c3d46cdcc11d1901cf2aedb
Mike MacCana <mike.maccana@gmail.com>
2024-01-26 15:44:48 -0500

    Fix tests: remove mocha, chai, yarn lock

--- tests/anchor-wtf.test.ts
+++ tests/anchor-wtf.test.ts
@@ -1,16 +1,21 @@
+import { before, describe, test } from "node:test";
+import assert from "node:assert/strict";
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorWtf } from "../target/types/anchor_wtf";
+const log = console.log;
 
 describe("anchor-wtf", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.AnchorWtf as Program<AnchorWtf>;
+  const instructionHandlers = program.methods;
 
-  it("Is reveald!", async () => {
+  test("reveal works", async () => {
     // Add your test here.
-    const tx = await program.methods.reveal().rpc();
-    console.log("Your transaction signature", tx);
+    const transactionSignature = await instructionHandlers.reveal().rpc();
+    log("Your transaction signature:", transactionSignature);
+    assert(transactionSignature);
   });
 });
