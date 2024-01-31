a3e271e24b981a6333e349b815f093df4ada664c
Mike MacCana <mike.maccana@gmail.com>
2024-01-31 14:14:40 -0500

    WIP idea for getProgramAddressAndBump()

--- tests/revealer.test.ts
+++ tests/revealer.test.ts
@@ -21,6 +21,30 @@ import {
 } from "./encoder";
 const log = console.log;
 
+const getProgramAddressAndBump = (
+  // @ts-ignore this type exists
+  inputs: Array<string | PublicKey | BN>,
+  programId: PublicKey
+) => {
+  const buffers = inputs.map((input) => {
+    if (typeof input === "string") {
+      return Buffer.from(input);
+    }
+    if (input instanceof PublicKey) {
+      return input.toBuffer();
+    }
+    // @ts-ignore this type exists
+    if (input instanceof BN) {
+      return input.toBuffer();
+    }
+  });
+  const programAddressAndBump = PublicKey.findProgramAddressSync(
+    buffers,
+    programId
+  );
+  return programAddressAndBump;
+};
+
 describe("encoding JS objects to arrays of numbers", () => {
   test("string to array of numbers", () => {
     const input = "hello world";
@@ -90,6 +114,12 @@ describe("revealer", async () => {
     // Needs to match instructions/reveal.ts
     //
     // TODO: make a generic function to take strings, publickeys, and BNs and turn them into buffers
+    // const programAddressAndBump = getProgramAddressAndBump(
+    //   ["revelation", sender.publicKey, id],
+    //   program.programId
+    // );
+    // const revelation = programAddressAndBump[0];
+
     const revelation = PublicKey.findProgramAddressSync(
       [
         Buffer.from("revelation"),
